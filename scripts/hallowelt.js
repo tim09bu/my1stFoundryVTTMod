a081e8362f0813ce768c31ea6b20333ab7268dde
tim09bu <tim09bu@gmail.com>
2022-05-15 16:32:25 +0200

    fixed the sound

--- scripts/hallowelt.js
+++ scripts/hallowelt.js
@@ -1,11 +1,11 @@
 console.log("Hello World! This code runs immediately when the file is loaded.");
 var damage_1 = new Audio('sounds/damage_sound_1.mp3');
-damage_1.play();
 
 Hooks.on("init", function() {
   console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
 });
 
 Hooks.on("ready", function() {
+  damage_1.play();
   console.log("This code runs once core initialization is ready and game data is available.");
 });
