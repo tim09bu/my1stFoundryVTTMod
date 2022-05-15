0bb59cd737a5b8ca9be5e4e329e33eac67338264
tim09bu <tim09bu@gmail.com>
2022-05-15 16:26:44 +0200

    added damage sound

--- scripts/hallowelt.js
+++ scripts/hallowelt.js
@@ -1,9 +1,9 @@
 console.log("Hello World! This code runs immediately when the file is loaded.");
+var damage_1 = new Audio('sounds/damage_sound_1.mp3');
+audio.play();
 
 Hooks.on("init", function() {
   console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
-  var damage_1 = new Audio('sounds/damage_sound_1.mp3');
-  audio.play();
 });
 
 Hooks.on("ready", function() {
